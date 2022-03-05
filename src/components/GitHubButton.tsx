78489c4cf9aa517c1d94ef2eca25e6de586d4750
jonoman55 <jonoman55@yahoo.com>
2022-03-05 18:51:21 -0500

    github link fixed

--- src/components/GitHubButton.tsx
+++ src/components/GitHubButton.tsx
@@ -4,7 +4,7 @@ import { GitHubLogoIcon } from '@modulz/radix-icons';
 const GitHubButton: React.FC = () => (
     <Button
         component='a'
-        href='https://github.com/https://github.com/jonoman55/mantine-ui-app'
+        href='https://github.com/jonoman55/mantine-ui-app/'
         target='_blank'
         leftIcon={<GitHubLogoIcon />}
         variant='gradient'

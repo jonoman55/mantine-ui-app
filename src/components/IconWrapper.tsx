869b240ab74ed46e8484040aaa3a69f3c12f03a9
jonoman55 <jonoman55@yahoo.com>
2022-03-05 16:42:43 -0500

    icon interface fix

--- src/components/IconWrapper.tsx
+++ src/components/IconWrapper.tsx
@@ -1,11 +1,11 @@
 import { ActionIcon, MantineNumberSize, ActionIconVariant, MantineColor } from '@mantine/core';
 
 interface IconProps {
-    size: MantineNumberSize | undefined,
-    color: string | MantineColor,
-    radius: MantineNumberSize | undefined,
-    variant: ActionIconVariant | undefined,
-    children: any
+    size: MantineNumberSize | undefined;
+    color: string | MantineColor;
+    radius: MantineNumberSize | undefined;
+    variant: ActionIconVariant | undefined;
+    children: any;
 };
 
 const IconWrapper = ({ size, color, radius, variant, children }: IconProps) => (
